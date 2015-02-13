2458a27367a4fd2128ebf556292b5319c399f2a1
Alexander Pilz <pilz@syslab.com>
2015-02-13 12:35:53 +0100

    slc release

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ class PyTest(TestCommand):
 
 setup(
     name='raven',
-    version='5.2.0',
+    version='5.2.0.recursion',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/getsentry/raven-python',
